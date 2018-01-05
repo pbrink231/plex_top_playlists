e888af46cfa478c2ff5b408d59025f30529d2e7e
gkspranger <greg@spranger.us>
2018-01-05 13:56:03 -0500

    test scripts

---
+++ test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+USERS = []
+NOT_USERS = [
+    "frank"
+]
+
+def not_user(user):
+    if (not USERS or user in USERS) and user not in NOT_USERS:
+        print "adding user: {0}".format(
+            user
+        )
+    else:
+        print "not adding user: {0}".format(
+            user
+        )
+
+if __name__ == "__main__":
+    not_user("greg")
+    not_user("frank")
